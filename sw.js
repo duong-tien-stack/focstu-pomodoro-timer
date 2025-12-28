6ff43d6871a4e5d3266909d6643d7bfd9960d534
duong-tien-stack <heyeayyaey@gmail.com>
2025-12-28 13:12:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,39 @@
-importScripts("https://cdn.pushalert.co/sw-86876.js");
+const cacheName = "clock";
+const contentToCache = [
+	"./",
+	"./index.html",
+	"./index.css",
+	"./index.js",
+	"./worker.js",
+	"./sw.js",
+	"./icons/logo-icon32.png",
+	"./icons/logo-icon192.png",
+	"./icons/logo-icon512.png",
+	"./icons/maskable-icon192.png",
+	"./icons/maskable-512.png",
+	"./site.webmanifest",
+	"./icons/icon180.png",
+];
+
+self.addEventListener("install", (e) => {
+	console.log("Service Worker installed");
+	e.waitUntil(
+		(async () => {
+			const cache = await caches.open(cacheName);
+			await cache.addAll(contentToCache).catch(err => console.log(err));
+		})()
+	);
+});
+
+self.addEventListener("fetch", function (event) {
+	event.respondWith(fetch(event.request).then((res) => {
+		let response = res.clone();
+        caches.open(cacheName).then((cache) => {
+          cache.put(event.request, response);
+        });
+		return res
+	}).catch((err) => {
+		return caches.match(event.request)
+	})
+	);
+});
